9b6fee0db1fe7c322558c761601b1113d79b8739
mitanisiwi <131637872+mitanisiwi@users.noreply.github.com>
2023-09-01 13:30:16 +0900

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bulletin-board-client-nu.vercel.app'、"http://localhost:3000"
+    origins 'https://bulletin-board-client-nu.vercel.app',"http://localhost:3000"
 
     resource "*",
       headers: :any,
